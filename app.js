0d68468e412c6f0ed1c9b5b0ce42905e7adbc00f
Holly Ford <holly_ford@hotmail.com>
2021-11-01 16:24:50 +0000

    created main HTML and JS so page is functional

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-const eventWhichMain = document.getElementById("eventWhichMain")
-const eventKey = document.getElementById("eventKey")
-const eventWhich = document.getElementById("eventWhich")
-const eventCode = document.getElementById("eventCode")
+const main = document.getElementById("main")
+const key = document.getElementById("key")
+const which = document.getElementById("which")
+const code = document.getElementById("code")
 
 window.addEventListener("keypress", (event) => {
-    console.log(event.which)
+    console.log(event);
+    main.textContent = `${event.which}`;
+    which.textContent = `${event.which}`;
+    key.textContent = `${event.key}`;
+    code.textContent = `${event.code}`;
 })
+
+
