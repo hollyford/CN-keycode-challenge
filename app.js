5a39298edcc6b2232b07fa5bc89298db678d6431
Holly Ford <holly_ford@hotmail.com>
2021-11-01 15:58:21 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const eventWhichMain = document.getElementById("eventWhichMain")
+const eventKey = document.getElementById("eventKey")
+const eventWhich = document.getElementById("eventWhich")
+const eventCode = document.getElementById("eventCode")
+
+window.addEventListener("keypress", (event) => {
+    console.log(event.which)
+})
