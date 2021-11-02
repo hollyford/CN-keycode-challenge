907727ec9f57e144c2e562b2427ac6fef4052645
Holly Ford <holly_ford@hotmail.com>
2021-11-02 09:34:32 +0000

    removed console log from JS file

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const which = document.getElementById("which")
 const code = document.getElementById("code")
 
 window.addEventListener("keypress", (event) => {
-    console.log(event);
     main.textContent = `${event.which}`;
     which.textContent = `${event.which}`;
     key.textContent = `${event.key}`;
